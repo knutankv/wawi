e56ca36f81ec0e6cfbb4c8d704f7ff6cecd5f03f
akselfenerci <35256221+akselfenerci@users.noreply.github.com>
2025-01-30 12:28:05 +0100

    deleted unnecessary file

--- tests/run_tests.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-pytest.main(["-v", "c:/Users/aksef/Documents/GitHub/wawi/tests/test_dummy.py"])
-
-pytest.main(["-v", "c:/Users/aksef/Documents/GitHub/wawi/tests/test_IABSE_step11a.py"])
