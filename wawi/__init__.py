308c4fd44606bf07b5e93f4500fb1103c46c4325
Knut Andreas Kvåle <knut.a.kvale@ntnu.no>
2025-01-30 11:15:58 +0100

    First commit - version 0.0.1

--- wawi/__init__.py
+++ wawi/__init__.py
@@ -1,30 +1,7 @@
-# Include packages as standard
-# from .general import *
-# from wawi import abq
+__version__ = "0.0.1"
 
 # Other packages
 import numpy as np
 import shutil
 import csv
 from copy import deepcopy
-
-#
-#
-# HOW TO USE MODULE IMPORT IN __init__.py FILE.
-#
-#
-# Unpack all functions in module to base level (package.*):
-# from module import *
-#
-# Unpack specific function in module to base level (package.function):
-# from module import function
-#
-# Import full module, and keep module structure (package.module.function):
-# import module
-#
-# The __all__ array defines what modules are imported when from package import *
-# is used.
-#
-# If possible conflicts with global names/packages, use dot:
-# from .module import function/*
-#
